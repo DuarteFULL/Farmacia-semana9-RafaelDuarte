f625264f13b9e9b420c20bf3300306ad58e242fa
Rafael Duarte <rafael@prismarede.com.br>
2024-03-11 22:14:11 -0300

    Project Created

---
+++ farmacia/src/test/java/br/com/semana9/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.semana9.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
