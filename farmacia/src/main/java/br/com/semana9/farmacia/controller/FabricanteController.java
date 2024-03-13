e0f115777c67ac618226c9223629f7b76c3f65ef
Rafael Duarte <rafael@prismarede.com.br>
2024-03-12 22:11:01 -0300

    FabricanteController

--- farmacia/src/main/java/br/com/semana9/farmacia/controller/FabricanteController.java
+++ farmacia/src/main/java/br/com/semana9/farmacia/controller/FabricanteController.java
@@ -13,31 +13,4 @@ public class FabricanteController {
 	public void cadastrar(@RequestBody String json){
 		System.out.println(json);
 	}
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String nome;
-	
-	public Fabricante() {
-	}
-	
-	public Fabricante(String nome) {
-		this.nome = nome;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public String toString() {
-		return "[nome=" + nome + "]";
-	}
-
-	
-
 }
