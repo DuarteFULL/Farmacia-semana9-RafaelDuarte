631381b067d358727a17c662b51dbf401f3a5d49
Rafael Duarte <rafael@prismarede.com.br>
2024-03-16 17:02:37 -0300

    Validations

--- farmacia/src/main/java/br/com/semana9/farmacia/dto/FabricanteRecord.java
+++ farmacia/src/main/java/br/com/semana9/farmacia/dto/FabricanteRecord.java
@@ -2,6 +2,8 @@ package br.com.semana9.farmacia.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record FabricanteRecord(@NotBlank String nome) {
+public record FabricanteRecord(
+    @NotBlank String nome
+    ) {
 
-}
+    }
--- farmacia/src/main/java/br/com/semana9/farmacia/dto/ProdutoRecord.java
+++ farmacia/src/main/java/br/com/semana9/farmacia/dto/ProdutoRecord.java
@@ -1,16 +1,14 @@
 package br.com.semana9.farmacia.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record ProdutoRecord(
         @NotBlank String nome,
-        @NotBlank @Email String descricao,
+        @NotBlank String descricao,
         @NotBlank Float preco,
-        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
         @NotNull @Valid FabricanteRecord fabricante
-) {
-}
+        ){
+                
+        }
